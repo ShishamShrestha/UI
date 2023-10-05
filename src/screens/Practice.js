340252b1a4ed8586e0260e8b7712ccc111870b04
Sanjit <mail@mail.com>
2023-10-05 13:22:49 +0545

    practice

---
+++ src/screens/Practice.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <View>
+      <View style={styles.first} ><Text>Hlelo</Text></View>
+      <View/>
+      <View/> 
+      <View/>
+    </View>
+  )
+}
+
+export default Practice
+
+const styles = StyleSheet.create({
+    first:{
+        height:200,
+        width:200,
+        borderRadius:20,
+        backgroundColor:'red',
+        alignContent:'space-between'
+
+    }
+})
